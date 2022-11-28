8af6a29ee36bb6230b8d3814a22cb88f98c78439
Dibya <dash.dibyadebayan789@gmail.com>
2022-11-28 20:56:48 +0530

    Added  lazy loading to the image tag

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 var card_list = document.getElementsByClassName("cards");
-var setLimit = 12;
+var setLimit = 1000;
 const api = `https://pokeapi.co/api/v2/pokemon?limit=${setLimit}&offset=0`;
 
+
+function CreatePokemonCard(pokemon) {
+  const pokemonCard = document.createElement("div");
+  pokemonCard.innerHTML = `
+          <img loading="lazy" src="https://img.pokemondb.net/artwork/large/${pokemon.name}.jpg">
+          <h3>${pokemon.name}</h3> 
+          <p class="health"><span>Health</span> : 100</p>
+          <p class="power"><span>Power</span> : 75</p>
+          <p class="type"> <span>Type</span>: Electric</p>
+  `;
+  pokemonCard.classList.add("card-container");
+  card_list[0].appendChild(pokemonCard);
+}
 // Display it to the dom
 function displayPokemons(pokemons) {
   pokemons.forEach((pokemon) => {
-    const pokemonCard = document.createElement("div");
-    pokemonCard.classList.add("card-container");
-    pokemonCard.innerHTML = `
-            <img src="https://img.pokemondb.net/artwork/large/${pokemon.name}.jpg">
-            <h3>${pokemon.name}</h3> 
-            <p class="health"><span>Health</span> : 100</p>
-            <p class="power"><span>Power</span> : 75</p>
-            <p class="type"> <span>Type</span>: Electric</p>
-    `;
-    card_list[0].appendChild(pokemonCard);
+    CreatePokemonCard(pokemon);
   });
 }
 // Function to fetch data from API
